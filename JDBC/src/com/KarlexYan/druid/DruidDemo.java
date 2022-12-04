02f8fee32484c5a04eb03b1254829e2582c1364e
KarlexYan <KarlexYan@aliyun.com>
2022-12-04 15:36:29 +0800

    upload druid demo

---
+++ JDBC/src/com/KarlexYan/druid/DruidDemo.java
@@ -0,0 +1,28 @@
+package com.KarlexYan.druid;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DruidDemo {
+    // Druid连接池
+    public static void main(String[] args) throws Exception {
+
+        // 加载配置文件
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("JDBC/src/druid.properties"));
+
+        // 获取连接池对象
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(prop);
+
+        // 获取数据库连接 Connection
+        Connection connection = dataSource.getConnection();
+
+        // 获取当前路径
+//        System.out.println(System.getProperty("user.dir"));
+
+    }
+}
