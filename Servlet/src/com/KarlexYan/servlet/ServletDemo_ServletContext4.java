0a5c908c1517a9ec850d6a4346feb051d96623c2
KarlexYan <KarlexYan@aliyun.com>
2022-12-09 23:43:25 +0800

    读取Web应用下的资源文件

---
+++ Servlet/src/com/KarlexYan/servlet/ServletDemo_ServletContext4.java
@@ -0,0 +1,34 @@
+package com.KarlexYan.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@WebServlet("/servletContext4")
+public class ServletDemo_ServletContext4 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 获取ServletContext对象
+        ServletContext servletContext = this.getServletContext();
+        // 返回映射到资源文件的InputStream输入流对象
+        InputStream io = servletContext.getResourceAsStream("WEB-INF/jjj.properties");
+        // 加载配置文件
+        Properties prop = new Properties();
+        prop.load(io);
+        String company = prop.getProperty("Company");
+        String address = prop.getProperty("Address");
+
+        resp.getWriter().write("Company:" + company + "\nAddress:" + address);
+    }
+}
