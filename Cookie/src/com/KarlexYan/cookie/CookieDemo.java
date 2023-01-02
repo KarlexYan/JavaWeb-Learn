6d72f581f700bad99c693454eb5fe48e12b70bf1
KarlexYan <KarlexYan@aliyun.com>
2023-01-02 15:53:27 +0800

    Cookie使用

---
+++ Cookie/src/com/KarlexYan/cookie/CookieDemo.java
@@ -0,0 +1,24 @@
+package com.KarlexYan.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/cookie/cookieDemo1")
+public class CookieDemo extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 创建Cookie
+        Cookie cookie = new Cookie("username", "zhangsan");
+
+    }
+}
