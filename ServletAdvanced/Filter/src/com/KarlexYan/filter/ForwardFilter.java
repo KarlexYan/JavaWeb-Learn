62c61a192640db4b3b2332def697444940db07df
KarlexYan <KarlexYan@aliyun.com>
2023-01-08 20:11:19 +0800

    dispatcherTypes

---
+++ ServletAdvanced/Filter/src/com/KarlexYan/filter/ForwardFilter.java
@@ -0,0 +1,25 @@
+package com.KarlexYan.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+// 过滤器设置dispatcherTypes = DispatcherType.FORWARD，如果通过RequestDispatcher对象的forward()方法访问目标资源，那么过滤器将被调用
+@WebFilter(filterName = "ForwardFilter", urlPatterns = "/first.jsp", dispatcherTypes = DispatcherType.FORWARD)
+public class ForwardFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init方法执行...");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("doFilter方法执行...");
+        servletResponse.getWriter().write("ForwardFilterTest");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy方法执行...");
+    }
+}
---
+++ ServletAdvanced/Filter/src/com/KarlexYan/servlet/ForwardServlet.java
@@ -0,0 +1,17 @@
+package com.KarlexYan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ForwardServlet", urlPatterns = "/forwardServlet")
+public class ForwardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 使用forward
+        req.getRequestDispatcher("/first.jsp").forward(req, resp);
+    }
+}
