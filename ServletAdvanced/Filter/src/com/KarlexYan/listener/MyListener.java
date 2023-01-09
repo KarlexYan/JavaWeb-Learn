af723eef7b8b0f260156af21585d287edaa6c357
KarlexYan <KarlexYan@aliyun.com>
2023-01-10 00:10:46 +0800

    Listener

---
+++ ServletAdvanced/Filter/src/com/KarlexYan/listener/MyListener.java
@@ -0,0 +1,40 @@
+package com.KarlexYan.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class MyListener implements ServletContextListener, ServletRequestListener, HttpSessionListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("ServletContext对象创建");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("ServletContext对象消亡");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        System.out.println("ServletRequest的创建");
+    }
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        System.out.println("ServletRequest的消亡");
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("HttpSession的创建");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("HttpSession的消亡");
+    }
+}
