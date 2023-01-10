0c34ca27645bcfd5d3fd4afdf0913928073abdea
KarlexYan <KarlexYan@aliyun.com>
2023-01-10 16:07:06 +0800

    更新结构

---
+++ ServletAdvance/src/com/KarlexYan/filter/AutoLoginFilter.java
@@ -0,0 +1,44 @@
+package com.KarlexYan.filter;
+
+import com.KarlexYan.domain.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class AutoLoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 获取cookie信息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        Cookie[] cookies = request.getCookies();
+        // 遍历cookie，查找autologin
+        String flag = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("autologin".equals(cookie.getName())) {
+                    flag = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+
+        if (flag != null) {
+            // 有，解析用户信息，封装成User对象，放行
+            String[] split = flag.split("-");
+            String username = split[0];
+            String password = split[1];
+            if ("karlexyan".equals(username) && "123".equals(password)) {
+                User user = new User(username, password);
+                request.getSession().setAttribute("user", user);
+            }
+        }
+
+        // 没有，也放行
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
---
+++ ServletAdvance/web/form.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: KarlexYan
+  Date: 2023/1/10
+  Time: 15:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>文件上传</title>
+</head>
+<body>
+<form action="" method="post" enctype="multipart/form-data">
+    <table width="600px">
+        <tr>
+            <td>上传者</td>
+            <td><input type="text" name="name"></td>
+        </tr>
+        <tr>
+            <td>上传文件</td>
+            <td><input type="file" name="file"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" value="上传"></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
